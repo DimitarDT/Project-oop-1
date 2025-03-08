8a092c4700e08b76af0515e5ec6bdfd2910a3e23
DimitarDT <dimitrtodorov695@gmail.com>
2025-03-08 16:14:25 +0200

    Added some initial files for project
    Added the Application class for the main method, added classes for jedi and planet and an enum for jedi ranks.

---
+++ src/bg/tu_varna/sit/a1/f23621650/Planet.java
@@ -0,0 +1,9 @@
+package bg.tu_varna.sit.a1.f23621650;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Planet {
+    private String name;
+    List<Jedi> jediList = new ArrayList<Jedi>();
+}
