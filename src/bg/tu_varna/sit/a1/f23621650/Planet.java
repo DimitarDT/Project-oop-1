50deed007b633fa9513b6b7dca4c96f023130693
Mitko <dimitrtodorov695@gmail.com>
2025-03-23 23:58:34 +0200

    Reworked class StarWarsUniverse Changed list in StarWarsUniverse to map and reworked methods

--- src/bg/tu_varna/sit/a1/f23621650/Planet.java
+++ src/bg/tu_varna/sit/a1/f23621650/Planet.java
@@ -15,20 +15,42 @@ public class Planet {
         this.name = name;
     }
 
-    public void addJedi(Jedi jedi)
-    {
-        jediList.add(jedi);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false; //getclass or getinstance?
+        Planet planet = (Planet) o;
+        return name.equals(planet.name);
     }
 
-    public void removeJedi(String name)
+    @Override
+    public int hashCode() {
+        return name.hashCode(); //Objects version 1.7??
+    }
+
+    public boolean addJedi(Jedi jedi)
+    {
+        for(Jedi jedi1: jediList)
+        {
+            if(jedi.getJediName().equals(jedi1.getJediName()))
+            {
+                return false;
+            }
+        }
+        jediList.add(jedi);
+        return true;
+    }
+
+    public boolean removeJedi(String name)
     {
         for(Jedi jedi: jediList)
         {
-            if(jedi.getJediName() == name)
+            if(jedi.getJediName().equals(name))
             {
                 jediList.remove(jedi);
-                break;
+                return true;
             }
         }
+        return false;
     }
 }
--- src/bg/tu_varna/sit/a1/f23621650/StarWarsUniverse.java
+++ src/bg/tu_varna/sit/a1/f23621650/StarWarsUniverse.java
@@ -1,7 +1,9 @@
 package bg.tu_varna.sit.a1.f23621650;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class StarWarsUniverse {
     //Singleton implementation
@@ -16,34 +18,62 @@ public class StarWarsUniverse {
         return starWarsUniverse;
     }
 
-    List<Planet> planets = new ArrayList<Planet>(); // is it okay for this to be placed here?
+    Map<String, Planet> planets = new HashMap<String, Planet>(); // is it okay for this to be placed here?
 
     public void addPlanet(String planetName)
     {
-        planets.add(new Planet(planetName));
-    }//Message for already existing planets and jedi (contains and equals?)
+        if(planets.containsKey(planetName))
+        {
+            System.out.println("This planet already exists!");
+        }
+        else
+        {
+            planets.put(planetName, new Planet(planetName));
+        }
+    }
 
     public void addJediToPlanet(String planetName, String jediName, JediRank jediRank, int age, String lightsaberColor, double strength)
     {
-        for(Planet planet: planets)
+        if(!(planets.containsKey(planetName)))
         {
-            if(planet.getName() == planetName)
-            {
-                planet.addJedi(new Jedi(jediName, jediRank, age, lightsaberColor,strength));//a lot of arguments are being repeated here
-                break;
-            } //add message for when there is no such planet or if there is already such a jedi
+            System.out.println("Jedi creation wasn't successful. There is no such planet.");
+            return;
+        }
+
+        if(planets.get(planetName).addJedi(new Jedi(jediName, jediRank, age, lightsaberColor, strength))) //a lot of arguments are being repeated here
+        {
+            System.out.println("Jedi creation was successful.");
+        }
+        else
+        {
+            System.out.println("Jedi creation wasn't successful. There is already a jedi with this name on this or another planet.");
         }
     }// when is the jedi gonna be initialised from the command line arguments?
 
     public void removeJediFromPlanet(String jediName, String planetName)
     {
-        for(Planet planet: planets)
+        if(!(planets.containsKey(planetName)))
+        {
+            System.out.println("Jedi removal wasn't successful. There is no such planet.");
+            return;
+        }
+
+        if(planets.get(planetName).removeJedi(jediName))
+        {
+            System.out.println("Jedi was removed.");
+        }
+        else
+        {
+            System.out.println("There is no such jedi on this planet!");
+
+        }
+        /*for(Planet planet: planets)
         {
             if(planet.getName() == planetName)
             {
                 planet.removeJedi(jediName);
                 break;
             }
-        }
+        }*/
     }//add message for no such jedi/planet exists
 }
